0a596155610382784555436cb7ee477d1992906a
yu-i-i <kla9555@gmail.com>
2024-08-01 01:25:25 +0200

    Promisify `AuthenticationControllerLdap.doPassportLdapLogin`

--- services/web/modules/ldap-authentication/app/src/AuthenticationControllerLdap.js
+++ services/web/modules/ldap-authentication/app/src/AuthenticationControllerLdap.js
@@ -2,76 +2,62 @@ const AuthenticationManagerLdap = require('./AuthenticationManagerLdap')
 const AuthenticationController = require('../../../../app/src/Features/Authentication/AuthenticationController')
 const LoginRateLimiter = require('../../../../app/src/Features/Security/LoginRateLimiter')
 const logger = require('@overleaf/logger')
-const {
-  ParallelLoginError,
-} = require('../../../../app/src/Features/Authentication/AuthenticationErrors')
+const { handleAuthenticateErrors } = require('../../../../app/src/Features/Authentication/AuthenticationErrors')
 const Modules = require('../../../../app/src/infrastructure/Modules')
 
 const AuthenticationControllerLdap = {
-  doPassportLdapLogin(req, ldapUser, done) {
-    const  email = ldapUser.mail.toLowerCase()
-    Modules.hooks.fire(
-      'preDoPassportLogin',
-      req,
-      email,
-      function (err, infoList) {
-        if (err) {
-          return done(err)
-        }
-        const info = infoList.find(i => i != null)
-        if (info != null) {
-          return done(null, false, info)
-        }
-        LoginRateLimiter.processLoginRequest(email, function (err, isAllowed) {
-          if (err) {
-            return done(err)
-          }
-          if (!isAllowed) {
-            logger.debug({ email }, 'too many login requests')
-            return done(null, null, {
-              text: req.i18n.translate('to_many_login_requests_2_mins'),
-              type: 'error',
-              key: 'to-many-login-requests-2-mins',
-              status: 429,
-            })
-          }
-          const { fromKnownDevice } = AuthenticationController.getAuditInfo(req)
-          const auditLog = {
-            ipAddress: req.ip,
-            info: { method: 'LDAP login', fromKnownDevice },
-          }
-          AuthenticationManagerLdap.findOrCreateLdapUser(
-            ldapUser,
-            auditLog,
-            function (error, user) {
-              if (error != null) {
-                if (error instanceof ParallelLoginError) {
-                  return done(null, false, { status: 429 })
-                }
-                return done(error)
-              }
-              if (
-                user &&
-                AuthenticationController.captchaRequiredForLogin(req, user)
-              ) {
-                done(null, false, {
-                  text: req.i18n.translate('cannot_verify_user_not_robot'),
-                  type: 'error',
-                  errorReason: 'cannot_verify_user_not_robot',
-                  status: 400,
-                })
-              } else if (user) {
-                // async actions
-                done(null, user)
-              } else {  //something wrong
-                logger.debug({ email }, 'user is null')
-                done(null, false, { status: 500 } )
-              }
-            }
-          )
-        })
+  async doPassportLdapLogin(req, ldapUser, done) {
+    let user, info
+    try {
+      ;({ user, info } = await AuthenticationControllerLdap._doPassportLdapLogin(
+        req,
+        ldapUser
+      ))
+    } catch (error) {
+      return done(error)
+    }
+    return done(undefined, user, info)
+  },
+  async _doPassportLdapLogin(req, ldapUser) {
+    const email = ldapUser.mail.toLowerCase()
+    const { fromKnownDevice } = AuthenticationController.getAuditInfo(req)
+    const auditLog = {
+      ipAddress: req.ip,
+      info: { method: 'LDAP password login', fromKnownDevice },
+    }
+
+    let user, isPasswordReused
+    try {
+      user = await AuthenticationManagerLdap.promises.findOrCreateLdapUser(ldapUser, auditLog)
+    } catch (error) {
+      return {
+        user: false,
+        info: handleAuthenticateErrors(error, req),
       }
-    )
+    }
+    if (user && AuthenticationController.captchaRequiredForLogin(req, user)) {
+      return {
+        user: false,
+        info: {
+          text: req.i18n.translate('cannot_verify_user_not_robot'),
+          type: 'error',
+          errorReason: 'cannot_verify_user_not_robot',
+          status: 400,
+        },
+      }
+    } else if (user) {
+      // async actions
+      return { user, info: undefined }
+    } else { //something wrong
+      logger.debug({ email }, 'failed LDAP log in')
+      return {
+        user: false,
+        info: {
+          type: 'error',
+          status: 500,
+        },
+      }
+    }
   },
 }
 
