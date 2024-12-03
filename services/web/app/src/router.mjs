ad3da97aa54281ea8ea2313ed0bf6dd6c32fdf26
yu-i-i <kla9555@gmail.com>
2024-12-03 16:40:27 +0100

    Redirect non-existing links to Overleaf page

--- services/web/app/src/router.mjs
+++ services/web/app/src/router.mjs
@@ -1290,6 +1290,10 @@ async function initialize(webRouter, privateApiRouter, publicApiRouter) {
     TokenAccessController.grantTokenAccessReadOnly
   )
 
+  webRouter.get(['/learn*', '/templates*', '/blog*', '/latex*', '/for/*', '/contact*'], (req, res) => {
+    res.redirect(301, `https://www.overleaf.com${req.originalUrl}`)
+  })
+
   webRouter.get('/unsupported-browser', renderUnsupportedBrowserPage)
 
   webRouter.get('*', ErrorController.notFound)
