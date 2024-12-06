a66d4389bf28f6b22700421a134043c1a6c6f384
yu-i-i <kla9555@gmail.com>
2024-12-06 12:45:15 +0100

    Allow selecting a TeX Live image for a project

--- services/web/app/src/Features/Project/ProjectOptionsHandler.mjs
+++ services/web/app/src/Features/Project/ProjectOptionsHandler.mjs
@@ -39,7 +39,6 @@ const ProjectOptionsHandler = {
     if (!imageName || !Array.isArray(settings.allowedImageNames)) {
       return undefined
     }
-    imageName = imageName.toLowerCase()
     const isAllowed = settings.allowedImageNames.find(
       allowed => imageName === allowed.imageName
     )
