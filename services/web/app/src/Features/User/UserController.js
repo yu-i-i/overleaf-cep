c1c35ae20fe72b00de521fc10811ffc0c11b6ddd
yu-i-i <kla9555@gmail.com>
2024-12-17 18:36:18 +0100

    Refactor authentication code; add OIDC support

--- services/web/app/src/Features/User/UserController.js
+++ services/web/app/src/Features/User/UserController.js
@@ -401,7 +401,7 @@ async function updateUserSettings(req, res, next) {
   if (
     newEmail == null ||
     newEmail === user.email ||
-    (req.externalAuthenticationSystemUsed() && !user.hashedPassword)
+    req.externalAuthenticationSystemUsed()
   ) {
     // end here, don't update email
     SessionManager.setInSessionUser(req.session, {
@@ -478,7 +478,6 @@ async function doLogout(req) {
 }
 
 async function logout(req, res, next) {
-  if (req?.session.saml_extce) return res.redirect(308, '/saml/logout')
   const requestedRedirect = req.body.redirect
     ? UrlHelper.getSafeRedirectPath(req.body.redirect)
     : undefined
