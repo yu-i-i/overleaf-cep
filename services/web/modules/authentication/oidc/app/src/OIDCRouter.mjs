36719cbf3a406cd5fe24662ea567adf82e892543
yu-i-i <kla9555@gmail.com>
2025-02-22 03:26:25 +0100

    Whitelist /oidc/login endpoint, fixes #21

--- services/web/modules/authentication/oidc/app/src/OIDCRouter.mjs
+++ services/web/modules/authentication/oidc/app/src/OIDCRouter.mjs
@@ -1,5 +1,6 @@
 import logger from '@overleaf/logger'
 import UserController from '../../../../../app/src/Features/User/UserController.js'
+import AuthenticationController from '../../../../../app/src/Features/Authentication/AuthenticationController.js'
 import OIDCAuthenticationController from './OIDCAuthenticationController.mjs'
 import logout from '../../../logout.mjs'
 
@@ -7,7 +8,9 @@ export default {
   apply(webRouter) {
     logger.debug({}, 'Init OIDC router')
     webRouter.get('/oidc/login', OIDCAuthenticationController.passportLogin)
+    AuthenticationController.addEndpointToLoginWhitelist('/oidc/login')
     webRouter.get('/oidc/login/callback', OIDCAuthenticationController.passportLoginCallback)
+    AuthenticationController.addEndpointToLoginWhitelist('/oidc/login/callback')
     webRouter.get('/oidc/logout/callback', OIDCAuthenticationController.passportLogoutCallback)
     webRouter.post('/user/oauth-unlink', OIDCAuthenticationController.unlinkAccount)
     webRouter.post('/logout', logout, UserController.logout)
