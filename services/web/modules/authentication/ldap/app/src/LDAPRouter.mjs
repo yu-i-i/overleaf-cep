9bc7fcf77ad0581f5568e96bb776afbc666f812a
yu-i-i <kla9555@gmail.com>
2025-03-10 05:55:01 +0100

    See upstream commit 42ee56e

--- services/web/modules/authentication/ldap/app/src/LDAPRouter.mjs
+++ services/web/modules/authentication/ldap/app/src/LDAPRouter.mjs
@@ -10,7 +10,7 @@ export default {
     logger.debug({}, 'Init LDAP router')
     webRouter.post('/login',
       RateLimiterMiddleware.rateLimit(overleafLoginRateLimiter), // rate limit IP (20 / 60s)
-      RateLimiterMiddleware.loginRateLimitEmail, // rate limit email (10 / 120s)
+      RateLimiterMiddleware.loginRateLimitEmail(), // rate limit email (10 / 120s)
       CaptchaMiddleware.validateCaptcha('login'),
       LDAPAuthenticationController.passportLogin,
       AuthenticationController.passportLogin,
