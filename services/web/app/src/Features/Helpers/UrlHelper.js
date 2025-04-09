02ddac0b3f2db18d7a4b414a56cf82f16ebefefc
yu-i-i <kla9555@gmail.com>
2025-04-09 23:50:50 +0200

    Enable From External URL feature

--- services/web/app/src/Features/Helpers/UrlHelper.js
+++ services/web/app/src/Features/Helpers/UrlHelper.js
@@ -32,14 +32,6 @@ module.exports = {
   getCanonicalURL,
   getSafeRedirectPath,
   getSafeAdminDomainRedirect,
-  wrapUrlWithProxy(url) {
-    // TODO: Consider what to do for Community and Enterprise edition?
-    if (!Settings.apis.linkedUrlProxy.url) {
-      throw new Error('no linked url proxy configured')
-    }
-    return `${Settings.apis.linkedUrlProxy.url}?url=${encodeURIComponent(url)}`
-  },
-
   prependHttpIfNeeded(url) {
     if (!url.match('://')) {
       url = `http://${url}`
