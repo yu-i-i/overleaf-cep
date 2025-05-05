c7350d58777a27b3fd5d6dfd23e6e15589966c8f
yu-i-i <kla9555@gmail.com>
2025-05-05 03:27:23 +0200

    Symbol Palette: improve keyboard input experience

--- services/web/frontend/js/features/symbol-palette/components/symbol-palette-close-button.js
+++ services/web/frontend/js/features/symbol-palette/components/symbol-palette-close-button.js
@@ -1,19 +1,29 @@
 import { useEditorContext } from '../../../shared/context/editor-context'
 import { useTranslation } from 'react-i18next'
+import PropTypes from 'prop-types'
 
 export default function SymbolPaletteCloseButton() {
   const { toggleSymbolPalette } = useEditorContext()
   const { t } = useTranslation()
+
+  const handleClick = () => {
+    toggleSymbolPalette()
+    window.dispatchEvent(new CustomEvent('editor:focus'))
+  }
 
   return (
     <div className="symbol-palette-close-button-outer">
       <button
         type="button"
         className="btn-close symbol-palette-close-button"
-        onClick={toggleSymbolPalette}
+        onClick={handleClick}
         aria-label={t('close')}
       >
       </button>
     </div>
   )
+}
+
+SymbolPaletteCloseButton.propTypes = {
+  focusInput: PropTypes.func,
 }
