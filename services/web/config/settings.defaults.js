103702626fbfd9a641c95a906d96cdd277235902
yu-i-i <kla9555@gmail.com>
2025-05-13 17:13:27 +0200

    Refactor Sandboxed Compiles

--- services/web/config/settings.defaults.js
+++ services/web/config/settings.defaults.js
@@ -1070,6 +1070,7 @@ module.exports = {
     'launchpad',
     'server-ce-scripts',
     'user-activate',
+    'sandboxed-compiles',
     'symbol-palette',
     'track-changes',
     'authentication/ldap',
@@ -1102,15 +1103,6 @@ module.exports = {
   managedUsers: {
     enabled: false,
   },
-
-  allowedImageNames: process.env.SANDBOXED_COMPILES === 'true'
-    ? parseTextExtensions(process.env.ALL_TEX_LIVE_DOCKER_IMAGES)
-        .map((imageName, index) => ({
-          imageName,
-          imageDesc: parseTextExtensions(process.env.ALL_TEX_LIVE_DOCKER_IMAGE_NAMES)[index]
-            || imageName.split(':')[1],
-        }))
-    : undefined,
 
   oauthProviders: {
     ...(process.env.EXTERNAL_AUTH && process.env.EXTERNAL_AUTH.includes('oidc') && {
