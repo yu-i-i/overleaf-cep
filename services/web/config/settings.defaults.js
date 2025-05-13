4128241244db49bf9cd0afb7685bb965648c9796
yu-i-i <kla9555@gmail.com>
2025-05-13 17:13:27 +0200

    Refactor Sandboxed Compiles

--- services/web/config/settings.defaults.js
+++ services/web/config/settings.defaults.js
@@ -1005,6 +1005,7 @@ module.exports = {
     'launchpad',
     'server-ce-scripts',
     'user-activate',
+    'sandboxed-compiles',
   ],
   viewIncludes: {},
 
@@ -1032,14 +1033,6 @@ module.exports = {
     enabled: false,
   },
 
-  allowedImageNames: process.env.SANDBOXED_COMPILES === 'true'
-    ? parseTextExtensions(process.env.ALL_TEX_LIVE_DOCKER_IMAGES)
-        .map((imageName, index) => ({
-          imageName,
-          imageDesc: parseTextExtensions(process.env.ALL_TEX_LIVE_DOCKER_IMAGE_NAMES)[index]
-            || imageName.split(':')[1],
-        }))
-    : undefined,
 }
 
 module.exports.mergeWith = function (overrides) {
