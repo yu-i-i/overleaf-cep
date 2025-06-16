cc58958ff8d1eddb586f2668b23369723e68afe2
yu-i-i <kla9555@gmail.com>
2025-06-16 13:39:16 +0200

    Avoid DEP0174 by removing async from callback-based getGroupPolicyForUser

--- services/web/modules/authentication/saml/app/src/SAMLModuleManager.mjs
+++ services/web/modules/authentication/saml/app/src/SAMLModuleManager.mjs
@@ -84,18 +84,17 @@ const SAMLModuleManager = {
       logger.info({}, error.message)
     }
   },
-  async getGroupPolicyForUser(user, callback) {
-    try {
-      const userValidationMap = await PermissionsManager.promises.getUserValidationStatus({
-        user,
-        groupPolicy : { 'samlPolicy' : true },
-        subscription : null
-      })
+  getGroupPolicyForUser(user, callback) {
+    PermissionsManager.promises.getUserValidationStatus({
+      user,
+      groupPolicy : { 'samlPolicy' : true },
+      subscription : null
+    }).then(userValidationMap => {
       let groupPolicy = Object.fromEntries(userValidationMap)
-      callback(null,  {'groupPolicy' : groupPolicy })
-    } catch (error) {
+      callback(null, { groupPolicy })
+    }).catch(error => {
       callback(error)
-    }
+    })
   },
 }
 
