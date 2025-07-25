6ddd549442b7e89e33ae7999f08772640b1c572d
yu-i-i <kla9555@gmail.com>
2025-07-25 15:48:03 +0200

    Move Sympol Palette code to modules, closes #63

--- services/web/config/settings.defaults.js
+++ services/web/config/settings.defaults.js
@@ -1018,7 +1018,12 @@ module.exports = {
     pdfPreviewPromotions: [],
     diagnosticActions: [],
     sourceEditorCompletionSources: [],
-    sourceEditorSymbolPalette: ['@/features/symbol-palette/components/symbol-palette'],
+    sourceEditorSymbolPalette: [
+      Path.resolve(
+        __dirname,
+        '../modules/symbol-palette/frontend/components/symbol-palette'
+      ),
+    ],
     sourceEditorToolbarComponents: [],
     sourceEditorToolbarEndButtons: [],
     rootContextProviders: [],
