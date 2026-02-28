5c68279105dd7972768caba0d51710fe5ef04ab4
yu-i-i <kla9555@gmail.com>
2026-02-28 13:57:15 +0100

    Sandboxed compiles: set imageName to default if undefined in project

--- services/web/app/src/Features/Project/ProjectController.mjs
+++ services/web/app/src/Features/Project/ProjectController.mjs
@@ -563,6 +563,7 @@ const _ProjectController = {
           brandVariationId: 1,
           overleaf: 1,
           tokens: 1,
+          imageName: 1,
         }),
         userIsMemberOfGroupSubscription: sessionUser
           ? (async () =>
@@ -647,6 +648,8 @@ const _ProjectController = {
         projectId
       )
       const imageNames = await ProjectHelper.getAllowedImagesForUser(user)
+
+      if (!project.imageName) await EditorController.promises.setImageName(projectId, Settings.currentImageName)
 
       const privilegeLevel =
         await AuthorizationManager.promises.getPrivilegeLevelForProject(
